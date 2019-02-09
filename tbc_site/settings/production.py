b6ee7a19ce68c4407668d58594e37d2888b64f22
Vanessa Archambault <varch02@gmail.com>
2019-02-09 13:24:14 -0800

    set up AWS S3 asset management

--- tbc_site/settings/production.py
+++ tbc_site/settings/production.py
@@ -1,8 +1,52 @@
+from __future__ import absolute_import, unicode_literals
+
 from .base import *
+
+# Parse database configuration from $DATABASE_URL
+import os
+import dj_database_url
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Redirect all traffic to SSL via SecurityMiddleware
+SECURE_SSL_REDIRECT = True
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
 DEBUG = False
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+COMPRESS_OFFLINE = True
+COMPRESS_CSS_HASHING_METHOD = 'content'
+
+MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# See https://chrxr.com/django-error-logging-configuration-heroku/
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
+
 try:
-    from .local import *
+    pass
 except ImportError:
     pass
