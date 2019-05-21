f0e3f1b2ef669833791ef76f203ccbe7ee9535c4
Vanessa Archambault <varch02@gmail.com>
2019-05-20 17:05:31 -0700

    move header into tag

---
+++ home/templatetags/demo_tags.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals
+from django import template
+
+from wagtail.contrib.modeladmin.options import ModelAdmin
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def get_site_root(context):
+    # NB this returns a core.Page, not the implementation-specific model used
+    # so object-comparison to self will return false as objects would differ
+    return context['request'].site.root_page
+
+@register.inclusion_tag('home/tags/header.html', takes_context=True)
+def header(context, self):
+    self = self
+    return {
+        'self': self,
+        'request': context['request'],
+    }
